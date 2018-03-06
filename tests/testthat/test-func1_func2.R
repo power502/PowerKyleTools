7dc36531188b8e5f678b9d6b71433f756b9972f3
power502 <35541680+power502@users.noreply.github.com>
2018-03-06 14:09:05 -0600

    actually finished now

--- tests/testthat/test-func1_func2.R
+++ tests/testthat/test-func1_func2.R
@@ -15,4 +15,41 @@ test_that("func2 computes mean, var, sd", {
   save<-try(func2(NA),silent=TRUE)
   expect_identical(as.character(attr(save,"condition")),"Error: is.numeric(x) is not TRUE\n")
 })
+test_that("func3 computes the liklihood of a gamma distribution", {
+  dis <- rnorm(10)
+  alpha <- pi
+  log <- function(alpha)
+    sum(dgamma(dis, shape = alpha, log = TRUE))
+  interval <- mean(dis) + c(-1,1) * 3 * sd(dis)
+  interval <- pmax(mean(dis) / 1e3, interval)
+  
+  oout<- optimize(log, maximum = TRUE, interval)
+  xxx <- (oout$maximum)
+  expect_identical(xxx, func3(dis))
+})
 
+test_that("func4 computes the weighted mean, var, sd", {
+  d <- read.table(url("http://www.stat.umn.edu/geyer/3701/data/q1p4.txt"),header = TRUE)
+  a = sum(d$x * d$p)
+  b = sum(((d$x - a)^2) * d$p)
+  c = sqrt(b)
+  sol <- list(mean=a,var=b,sd=c)
+  
+  expect_identical(sol,func4(d))
+})
+
+test_that("func7 Computes the liklihood of a given distribution for data x", {
+  x1 = rgamma(100,3)
+  func1 = function(theta, x) dgamma(x, shape = theta, log = TRUE)
+  result7_gamma <- func7(x1,func1,c(0,3))
+  
+  x = x1
+  f7 <- function(theta, x){
+    sum(func1(theta, x))
+  }
+  interval = c(0,3)
+  oout <- optimize(f7, maximum = TRUE, interval, x=x)
+  final <- oout$maximum
+  
+  expect_identical(final, result7_gamma)
+})
