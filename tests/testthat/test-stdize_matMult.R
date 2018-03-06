6707f3d755e9dadbc86e727016cac95a1f95298c
power502 <35541680+power502@users.noreply.github.com>
2018-03-06 13:35:31 -0600

    ready for grading

--- tests/testthat/test-stdize_matMult.R
+++ tests/testthat/test-stdize_matMult.R
@@ -2,11 +2,26 @@ context("Quiz functions")
 
 test_that("matMult computes a scalar of a vector and matrix", {
   load(url("http://www.stat.umn.edu/geyer/3701/data/q2p1.rda"))
-  matMult(a, x)
+  inv <- solve(a)
+  p <-(t(x) %*% inv %*% x)
+  expect_identical(p,matMult(a, x))
          })
 
 test_that("stdize will standarize a matrix", {
   load(url("http://www.stat.umn.edu/geyer/3701/data/q2p1.rda"))
-  stdize(a)
+  count = 1
+  z <- a
+  rows <- nrow(z)
+  cols <- ncol(z)
+  while (count <= cols){
+    singleColumn <- z[,count]
+    columnMean <- mean(singleColumn)
+    columnSd <- sd(singleColumn)
+    z[,count] = ((singleColumn-columnMean)/columnSd)
+    count = count + 1
+  }
+  new <- z
+  
+  expect_identical(new,stdize(a))
 })
 
